2ca35495744c5d1267ac00ce1d2d5bb1de7dc7a3
Vaibhav <vaibhavshitole1303@gmail.com>
2024-07-17 16:32:42 +0530

    initial comment

--- lib/home/ui/exampleone.dart
+++ lib/home/ui/exampleone.dart
@@ -8,34 +8,33 @@ import '../model/postmodel.dart';
 class ExampleOne extends StatelessWidget {
   ExampleOne({super.key});
 
-  List<PostsModel> postList = [] ;
+  List<PostsModel> postList = [];
 
-  Future<List<PostsModel>> getPostApi ()async{
-    final resposne = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts')) ;
+  Future<List<PostsModel>> getPostApi() async {
+    final resposne =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
     var data = jsonDecode(resposne.body.toString());
-    if(resposne.statusCode == 200){
+    if (resposne.statusCode == 200) {
       postList.clear();
-      for(Map i in data){
+      for (Map i in data) {
         postList.add(PostsModel.fromJson(i));
       }
-      return postList ;
-    }else {
-      return postList ;
+      return postList;
+    } else {
+      return postList;
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: InkWell(
-          onTap: (){
-            Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>  HomePage(
-                                              )));
-          },
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => HomePage()));
+            },
             child: Text('Api Course')),
       ),
       body: Column(
@@ -43,13 +42,13 @@ class ExampleOne extends StatelessWidget {
           Expanded(
             child: FutureBuilder(
               future: getPostApi(),
-              builder: (context , snapshot){
-                if(!snapshot.hasData){
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
                   return Text('Loading');
-                }else {
+                } else {
                   return ListView.builder(
                       itemCount: postList.length,
-                      itemBuilder: (context, index){
+                      itemBuilder: (context, index) {
                         return Card(
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
@@ -57,14 +56,31 @@ class ExampleOne extends StatelessWidget {
                               mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-
-                                Text('Title' , style: TextStyle(fontSize: 15 , fontWeight: FontWeight.bold),),
-                                SizedBox(height: 3,),
+                                const Text(
+                                  'Title',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 3,
+                                ),
                                 Text(postList[index].title.toString()),
-                                SizedBox(height: 5,),
-                                Text('Description' , style: TextStyle(fontSize: 15 , fontWeight: FontWeight.bold),),
-                                SizedBox(height: 3,),
-                                Text('Description\n'+postList[index].body.toString() , style: Theme.of(context).textTheme.bodyText1)
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                const Text(
+                                  'Description',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 3,
+                                ),
+                                Text('Description\n${postList[index].body}',
+                                    style:
+                                        Theme.of(context).textTheme.bodyText1)
                               ],
                             ),
                           ),
